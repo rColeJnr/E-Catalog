365008efd4264cb14d7d84ce66229307602e6651
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-08 15:30:03 +0300

    movie details layout

--- app/src/main/java/com/rick/moviecatalog/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/com/rick/moviecatalog/fragments/MovieDetailsFragment.kt
@@ -1,6 +1,33 @@
 package com.rick.moviecatalog.fragments
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.rick.moviecatalog.databinding.FragmentMovieDetailsBinding
 
 class MovieDetailsFragment: Fragment() {
+
+    private var _binding: FragmentMovieDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMovieDetailsBinding.inflate(inflater, container, false)
+
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://www.nytimes.com/2022/05/05/movies/reflection-review.html")
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
