9d4f7267cfaeed70612b4f35a853438bb1cf40a8
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-08 11:51:59 +0300

    nav graph themes.xml and strings.xml

--- app/src/main/java/com/rick/moviecatalog/MainActivity.kt
+++ app/src/main/java/com/rick/moviecatalog/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.rick.moviecatalog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.rick.moviecatalog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragmentContainerView)
     }
 }
