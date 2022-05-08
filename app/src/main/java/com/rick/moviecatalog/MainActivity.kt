951554ac567205747a620a4e1b7a3bda4b5cd1a9
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-08 15:39:05 +0300

    app running, bugs fixed, code clean

--- app/src/main/java/com/rick/moviecatalog/MainActivity.kt
+++ app/src/main/java/com/rick/moviecatalog/MainActivity.kt
@@ -3,7 +3,6 @@ package com.rick.moviecatalog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.findNavController
 import com.rick.moviecatalog.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +17,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.fragmentContainerView)
+//        val navController = findNavController(R.id.fragmentContainerView)
     }
 }
--- app/src/main/java/com/rick/moviecatalog/MovieCatalogApplication.kt
+++ app/src/main/java/com/rick/moviecatalog/MovieCatalogApplication.kt
@@ -4,5 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MovieCatalogApplication: Application() {
-}
+class MovieCatalogApplication: Application()
