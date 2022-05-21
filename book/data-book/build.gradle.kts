441f48c0d462dcbf20a45c1e09da612d5b7de648
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-21 19:08:46 +0300

    book modules gradle configuration

--- book/data-book/build.gradle.kts
+++ book/data-book/build.gradle.kts
@@ -1,43 +1,23 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    compileSdk = 32
-
-    defaultConfig {
-        minSdk = 27
-        targetSdk = 32
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+apply {
+    from("$rootDir/kotlin-library.gradle")
+    plugin("kotlin-kapt")
+    plugin("kotlin-parcelize")
+    plugin("dagger.hilt.android.plugin")
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    "implementation"(project(":core"))
+
+    "implementation"(Retrofit.retrofit)
+    "implementation"(Retrofit.converter)
+    "implementation"(Retrofit.okhttp)
+    "implementation"(Retrofit.interceptor)
+
+    "implementation"(Hilt.android)
+    "kapt"(Hilt.compiler)
+
+    "implementation"(Room.roomRuntime)
+
+    "implementation"(Coroutines.core)
 }
