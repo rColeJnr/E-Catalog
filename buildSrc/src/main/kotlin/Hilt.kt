f326cacb94b640aab64398c48d7e040d26099e9a
rcolejnr <ricardo.m.jnr@gmail.com>
2022-05-21 17:44:32 +0300

    gradle upgrade

--- buildSrc/src/main/kotlin/Hilt.kt
+++ buildSrc/src/main/kotlin/Hilt.kt
@@ -1,11 +1,9 @@
 object Hilt {
-    const val hiltVersion = "2.42"
+    private const val hiltVersion = "2.42"
     const val android = "com.google.dagger:hilt-android:$hiltVersion"
     const val compiler = "com.google.dagger:hilt-compiler:$hiltVersion"
 
     const val hiltPlugin = "com.google.dagger:hilt-android-gradle-plugin:${hiltVersion}"
-}
 
-object HiltTest {
-    const val hiltAndroidTesting = "com.google.dagger:hilt-android-testing:${Hilt.hiltVersion}"
+    const val hiltAndroidTesting = "com.google.dagger:hilt-android-testing:${hiltVersion}"
 }
--- buildSrc/src/main/kotlin/Nav.kt
+++ buildSrc/src/main/kotlin/Nav.kt
@@ -3,5 +3,5 @@ object Nav {
     private const val nav_version = "2.4.2"
     const val navFrag = "androidx.navigation:navigation-fragment-ktx:$nav_version"
     const val navUI =  "androidx.navigation:navigation-ui-ktx:$nav_version"
-
+    const val navPlugin = "androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version"
 }
