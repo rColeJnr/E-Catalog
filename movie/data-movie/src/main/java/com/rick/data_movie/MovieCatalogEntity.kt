0e7eca3b78b483b30077543b9ee28135d49ac131
rcolejnr <ricardo.m.jnr@gmail.com>
2022-07-05 08:51:20 +0300

    fixed typo

--- movie/data-movie/src/main/java/com/rick/data_movie/MovieCatalogEntity.kt
+++ movie/data-movie/src/main/java/com/rick/data_movie/MovieCatalogEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class MovieCatalogEntitiy(
+data class MovieCatalogEntity(
     @PrimaryKey val id: Int? = null,
     val movieCatalog: List<Result>,
     val hasMore: Boolean
