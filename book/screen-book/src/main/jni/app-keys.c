dfeb174a1ca638a49177fbdd3336ade89a09254e
rcolejnr <ricardo.m.jnr@gmail.com>
2022-08-29 09:50:16 +0300

    shenanigans removed

--- book/screen-book/src/main/jni/app-keys.c
+++ book/screen-book/src/main/jni/app-keys.c
@@ -1,11 +1,1 @@
 #include <jni.h>
-
-//JNIEXPORT jstring JNICALL
-//Java_com_rick_data_1movie_MovieCatalogRepositoryKt_getNYKey(JNIEnv *env, jobject instance) {
-//    return (*env)->  NewStringUTF(env, "r2hGllYW5TVGnqspbO8u3li1Un4AlQgQ");
-//}
-//
-//JNIEXPORT jstring JNICALL
-//Java_com_rick_screen_1movie_MovieCatalogViewModelKt_getNYKey(JNIEnv *env, jobject instance) {
-//    return (*env)->  NewStringUTF(env, "r2hGllYW5TVGnqspbO8u3li1Un4AlQgQ");
-//}
--- movie/screen-movie/build.gradle.kts
+++ movie/screen-movie/build.gradle.kts
@@ -2,6 +2,8 @@ apply {
     from( "$rootDir/android-library.gradle")
 }
 
+
+
 dependencies {
 
     "implementation" (project(Modules.core))
