2da76de68fcea1d95ec8d56b62b62f2c037c3d77
rColeJnr <ricardo.m.jnr@gmail.com>
2022-09-01 15:36:56 +0300

    Delete app/src/main/jni directory

--- app/src/main/jni/app-keys.c
+++
@@ -1,6 +0,0 @@
-#include <jni.h>
-
-//JNIEXPORT jstring JNICALL
-//Java_com_rick_moviecatalog_MainActivityKt_getNYKey(JNIEnv *env, jclass clazz) {
-//    return (*env)->  NewStringUTF(env, "TmF0aXZlNWVjcmV0UEBzc3cwcmQx");
-//}
