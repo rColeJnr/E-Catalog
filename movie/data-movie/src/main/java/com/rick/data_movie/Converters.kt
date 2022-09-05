8b001c77b8a7f59bd5bd520d7b93688845ce5552
rcolejnr <ricardo.m.jnr@gmail.com>
2022-09-05 18:12:40 +0300

    type Converters.kt

--- movie/data-movie/src/main/java/com/rick/data_movie/Converters.kt
+++ movie/data-movie/src/main/java/com/rick/data_movie/Converters.kt
@@ -4,12 +4,13 @@ import androidx.room.ProvidedTypeConverter
 import androidx.room.TypeConverter
 import com.google.gson.reflect.TypeToken
 import com.rick.core.JsonParser
+import com.rick.data_movie.imdb.movie_model.*
 import com.rick.data_movie.ny_times.Link
 import com.rick.data_movie.ny_times.Movie
 import com.rick.data_movie.ny_times.Multimedia
 
 @ProvidedTypeConverter
-class Converters    (
+class Converters(
     private val jsonParser: JsonParser
 ) {
 
@@ -18,7 +19,7 @@ class Converters    (
     fun fromFeatureJson(json: String): List<Movie> =
         jsonParser.fromJson<ArrayList<Movie>>(
             json,
-            object : TypeToken<ArrayList<Movie>>(){}.type
+            object : TypeToken<ArrayList<Movie>>() {}.type
         ) ?: emptyList()
 
     // convert List<Result> to json
@@ -26,7 +27,7 @@ class Converters    (
     fun toFeatureJson(features: List<Movie>): String =
         jsonParser.toJson(
             features,
-            object : TypeToken<ArrayList<Movie>>(){}.type
+            object : TypeToken<ArrayList<Movie>>() {}.type
         ) ?: "[]"
 
     @TypeConverter
@@ -36,6 +37,7 @@ class Converters    (
     @TypeConverter
     fun fromStringToLink(string: String): Link =
         Link(string)
+
     @TypeConverter
     fun fromMultimediatoString(multimedia: Multimedia): String =
         multimedia.src
@@ -44,4 +46,75 @@ class Converters    (
     fun fromStringToMultimedia(string: String): Multimedia =
         Multimedia(string)
 
+    @TypeConverter
+    fun fromActorListToString(actorList: List<Actor>): String =
+        jsonParser.toJson(
+            actorList,
+            object : TypeToken<ArrayList<Actor>>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToActorList(actorList: String): List<Actor> =
+        jsonParser.fromJson<ArrayList<Actor>>(actorList,
+            object : TypeToken<ArrayList<Actor>>() {}.type
+        ) ?: emptyList()
+
+    @TypeConverter
+    fun fromDirectorListToString(directorList: List<Director>): String =
+        jsonParser.toJson(
+            directorList,
+            object : TypeToken<ArrayList<Director>>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToDirectorList(directorList: String): List<Director> =
+        jsonParser.fromJson<ArrayList<Director>>(directorList,
+            object : TypeToken<ArrayList<Director>>() {}.type
+        ) ?: emptyList()
+
+    @TypeConverter
+    fun fromRatingsToString(ratings: Ratings): String =
+        jsonParser.toJson(
+            ratings,
+            object : TypeToken<Ratings>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToRatings(ratings: String): Ratings =
+        jsonParser.fromJson<Ratings>(ratings,
+            object : TypeToken<Ratings>() {}.type
+        )!!
+
+    @TypeConverter
+    fun fromImagesToString(images: Images): String =
+        jsonParser.toJson(
+            images,
+            object : TypeToken<Images>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToImages(images: String): Images =
+        jsonParser.fromJson<Images>(images,
+            object : TypeToken<Images>() {}.type
+        )!!
+
+    @TypeConverter
+    fun fromOfficeToString(boxOffice: BoxOffice): String =
+        jsonParser.toJson(
+            boxOffice,
+            object : TypeToken<BoxOffice>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToOffice(boxOffice: String):BoxOffice =
+        jsonParser.fromJson<BoxOffice>(boxOffice,
+            object : TypeToken<BoxOffice>() {}.type
+        )!!
+
+    @TypeConverter
+    fun fromSimilarToString(similars: List<Similar>): String =
+        jsonParser.toJson(
+            similars,
+            object : TypeToken<ArrayList<Similar>>() {}.type
+        ) ?: "[]"
+
+    fun fromStringToSimilars(similars: String): List<Similar> =
+        jsonParser.fromJson<ArrayList<Similar>>(similars,
+            object : TypeToken<ArrayList<Similar>>() {}.type
+        ) ?: emptyList()
 }
