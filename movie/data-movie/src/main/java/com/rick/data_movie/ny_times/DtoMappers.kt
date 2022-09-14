2ea80db87440713b332f7df137180fa193b0d2ec
rcolejnr <ricardo.m.jnr@gmail.com>
2022-09-14 18:49:29 +0300

    little confusion between opening date and publicatication date

--- movie/data-movie/src/main/java/com/rick/data_movie/ny_times/DtoMappers.kt
+++ movie/data-movie/src/main/java/com/rick/data_movie/ny_times/DtoMappers.kt
@@ -9,7 +9,7 @@ fun ResultDto.toMovie(): Movie =
     Movie(
         title = display_title,
         summary = summary_short,
-        openingDate = opening_date,
+        openingDate = publication_date,
         rating = mpaa_rating,
         link = link.toLink(),
         multimedia = multimedia.toMultimedia()
