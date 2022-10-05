0ec86340635bddf3a91321c4220ef61e497286ba
rcolejnr <ricardo.m.jnr@gmail.com>
2022-10-05 21:43:21 +0300

    SeriesDetails double toolbar bug

--- app/src/main/java/com/rick/moviecatalog/MainActivity.kt
+++ app/src/main/java/com/rick/moviecatalog/MainActivity.kt
@@ -53,7 +53,9 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if (destination.id == com.rick.screen_movie.R.id.searchFragment
-                || destination.id == com.rick.screen_movie.R.id.movieDetailsFragment) {
+                || destination.id == com.rick.screen_movie.R.id.movieDetailsFragment
+                || destination.id == com.rick.screen_movie.R.id.seriesDetailsFragment
+            ) {
                 toolbar.visibility = View.GONE
             } else {
                 toolbar.visibility = View.VISIBLE
