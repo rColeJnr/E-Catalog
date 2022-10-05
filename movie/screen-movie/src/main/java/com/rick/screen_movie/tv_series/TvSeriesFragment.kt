906db7c4f6032d54a53f1927eefd20063f77e419
rcolejnr <ricardo.m.jnr@gmail.com>
2022-10-05 22:27:26 +0300

    navigation to SearchFragment

--- movie/screen-movie/src/main/java/com/rick/screen_movie/tv_series/TvSeriesFragment.kt
+++ movie/screen-movie/src/main/java/com/rick/screen_movie/tv_series/TvSeriesFragment.kt
@@ -1,9 +1,7 @@
 package com.rick.screen_movie.tv_series
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -11,7 +9,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.DefaultItemAnimator
+import com.google.android.material.transition.MaterialSharedAxis
 import com.rick.data_movie.imdb.series_model.TvSeries
+import com.rick.screen_movie.MovieCatalogFragmentDirections
 import com.rick.screen_movie.R
 import com.rick.screen_movie.databinding.FragmentMovieCatalogBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +23,11 @@ class TvSeriesFragment: Fragment() {
     private val binding get() = _binding!!
     private val viewModel: TvSeriesViewModel by viewModels()
     private lateinit var adapter: TvSeriesAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -66,6 +71,36 @@ class TvSeriesFragment: Fragment() {
         binding.recyclerView.adapter = adapter
         binding.recyclerView.itemAnimator = DefaultItemAnimator()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu, menu)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        menu.findItem(R.id.search_options).isVisible = false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.search_imdb -> {
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+                    duration = resources.getInteger(R.integer.catalog_motion_duration_long).toLong()
+                }
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+                    duration = resources.getInteger(R.integer.catalog_motion_duration_long).toLong()
+                }
+                val action =
+                    TvSeriesFragmentDirections.actionTvSeriesFragmentToSeriesSearchFragment(
+                        series = getString( R.string.tvseries_fragment ),
+                        movie = jnull
+                    )
+                findNavController().navigate(action)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroy() {
