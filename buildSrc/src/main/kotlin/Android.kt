dc2244b1cc48d63f96a2ba9084e8d8f2edcbf3e2
rcolejnr <ricardo.m.jnr@gmail.com>
2022-10-07 23:45:22 +0300

    33 sdk

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -2,7 +2,7 @@ object Android {
     const val appId = "com.rick.moviecatalog"
     const val compileSdk = 33
     const val minSdk = 24
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
 }
