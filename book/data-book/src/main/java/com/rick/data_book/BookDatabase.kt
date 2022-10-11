702d5e9e0ae0a4540bdfcacae5d651d676c60174
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-11 11:06:56 +0300

    type converters

--- book/data-book/src/main/java/com/rick/data_book/BookDatabase.kt
+++ book/data-book/src/main/java/com/rick/data_book/BookDatabase.kt
@@ -2,12 +2,14 @@ package com.rick.data_book
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [Book::class],
     version = 1,
     exportSchema = true
 )
+@TypeConverters(BookTypeConverters::class)
 abstract class BookDatabase: RoomDatabase() {
 
     abstract val bookDao: BookDao
---
+++ book/data-book/src/main/java/com/rick/data_book/BookTypeConverters.kt
@@ -0,0 +1,53 @@
+package com.rick.data_book
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.google.gson.reflect.TypeToken
+import com.rick.core.JsonParser
+
+@ProvidedTypeConverter
+class BookTypeConverters(
+    private val jsonParser: JsonParser
+) {
+    @TypeConverter
+    fun fromAuthorsToString(authors: List<Author>): String =
+        jsonParser.toJson(
+            authors,
+            object : TypeToken<List<Author>>() {}.type
+        ) ?: "[]"
+
+    @TypeConverter
+    fun fromStringToAuthors(authors: String): List<Author> =
+        jsonParser.fromJson<List<Author>>(
+            authors,
+            object : TypeToken<List<Author>>() {}.type
+        ) ?: emptyList()
+
+    @TypeConverter
+    fun fromFormatsToString(formats: Formats): String =
+        jsonParser.toJson(
+            formats,
+            object : TypeToken<Formats>() {}.type
+        ) ?: "[]"
+
+    @TypeConverter
+    fun fromStringToFormats(formats: String): Formats =
+        jsonParser.fromJson<Formats>(
+            formats,
+            object : TypeToken<Formats>() {}.type
+        )!!
+
+    @TypeConverter
+    fun fromTranslatorToString(translators: List<Translator>): String =
+        jsonParser.toJson(
+            translators,
+            object : TypeToken<List<Translator>>() {}.type
+        ) ?: "[]"
+
+    @TypeConverter
+    fun fromStringToTranslators(translators: String): List<Translator> =
+        jsonParser.fromJson<List<Translator>>(
+            translators,
+            object : TypeToken<List<Translator>>() {}.type
+        ) ?: emptyList()
+}
