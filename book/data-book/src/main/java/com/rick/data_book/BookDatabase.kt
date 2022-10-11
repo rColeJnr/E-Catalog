f1fc0aaf2f9fccd0c214f063b1a36d4bc20145ed
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-11 10:51:42 +0300

    DATABASE

--- book/data-book/src/main/java/com/rick/data_book/BookDatabase.kt
+++ book/data-book/src/main/java/com/rick/data_book/BookDatabase.kt
@@ -1,10 +1,19 @@
 package com.rick.data_book
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
-
+@Database(
+    entities = [Book::class],
+    version = 1,
+    exportSchema = true
+)
 abstract class BookDatabase: RoomDatabase() {
 
-    val bookDao: BookDao
+    abstract val bookDao: BookDao
+
+    companion object {
+        const val DATABASE_NAME = "BOOK_DB"
+    }
 
 }
