4cf72f60e3aeefdfbf50f2843c106ffbf70a8182
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-11 14:23:06 +0300

    PAGINGdapter

--- book/screen-book/src/main/java/com/rick/screen_book/BookCatalogAdapter.kt
+++ book/screen-book/src/main/java/com/rick/screen_book/BookCatalogAdapter.kt
@@ -1,35 +1,30 @@
 package com.rick.screen_book
 
 import android.view.ViewGroup
-import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.rick.data_book.model.Book
 
-class BookCatalogAdapter : RecyclerView.Adapter<BookCatalogViewHolder>() {
+class BookCatalogAdapter : PagingDataAdapter<Book, ViewHolder>(DIFF_UTIL) {
 
-    private val diffUtil = object : DiffUtil.ItemCallback<Book>() {
-        override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean =
-            oldItem.id == newItem.id
-
-
-        override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean =
-            oldItem == newItem
-
-    }
-
-    val differ = AsyncListDiffer(this, diffUtil)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookCatalogViewHolder {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: BookCatalogViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Book>() {
+            override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean =
+                oldItem == newItem
+
+        }
     }
 }
 
