721b5ffbfa03262c38db2f81c434c0f9541568d0
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-11 10:25:25 +0300

    di module

--- app/src/main/java/com/rick/moviecatalog/di/AppModule.kt
+++ app/src/main/java/com/rick/moviecatalog/di/AppModule.kt
@@ -1,72 +1,12 @@
 package com.rick.moviecatalog.di
 
-import android.content.Context
-import androidx.room.Room
-import com.google.gson.Gson
-import com.rick.core.GsonParser
-import com.rick.data_movie.MovieCatalogApi
-import com.rick.data_movie.MovieCatalogDatabase
-import com.rick.data_movie.imdb.IMDBApi
-import com.rick.data_movie.imdb.IMDBConverters
-import com.rick.data_movie.ny_times.Converters
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
-    @Provides
-    @Singleton
-    fun providesMovieCatalogApi(): MovieCatalogApi {
-        return Retrofit.Builder()
-            .baseUrl(MovieCatalogApi.NY_TIMES_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor(HttpLoggingInterceptor().apply {
-                        level = HttpLoggingInterceptor.Level.BASIC
-                    }).build()
-            )
-            .build()
-            .create()
-    }
-
-    @Provides
-    @Singleton
-    fun providesIMDBApi(): IMDBApi {
-        return Retrofit.Builder()
-            .baseUrl(IMDBApi.IMDB_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor(HttpLoggingInterceptor().apply {
-                        level = HttpLoggingInterceptor.Level.BASIC
-                    }).build()
-            )
-            .build()
-            .create()
-    }
-
-    @Provides
-    @Singleton
-    fun providesMovieDatabase(@ApplicationContext context: Context): MovieCatalogDatabase =
-        Room.databaseBuilder(
-            context,
-            MovieCatalogDatabase::class.java,
-            MovieCatalogDatabase.DATABASE_NAME
-        ).addTypeConverter(Converters(GsonParser(Gson())))
-            .addTypeConverter(IMDBConverters(GsonParser(Gson())))
-            .fallbackToDestructiveMigration()
-            .build()
 
 }
---
+++ movie/data-movie/src/main/java/com/rick/data_movie/DIModule.kt
@@ -0,0 +1,74 @@
+package com.rick.data_movie
+
+import android.content.Context
+import androidx.room.Room
+import com.google.gson.Gson
+import com.rick.core.GsonParser
+import com.rick.data_movie.imdb.IMDBApi
+import com.rick.data_movie.imdb.IMDBConverters
+import com.rick.data_movie.ny_times.Converters
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DIModule {
+    @Provides
+    @Singleton
+    fun providesMovieCatalogApi(): MovieCatalogApi {
+        return Retrofit.Builder()
+            .baseUrl(MovieCatalogApi.NY_TIMES_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BASIC
+                    }).build()
+            )
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesIMDBApi(): IMDBApi {
+        return Retrofit.Builder()
+            .baseUrl(IMDBApi.IMDB_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BASIC
+                    }).build()
+            )
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesMovieDatabase(@ApplicationContext context: Context): MovieCatalogDatabase =
+        Room.databaseBuilder(
+            context,
+            MovieCatalogDatabase::class.java,
+            MovieCatalogDatabase.DATABASE_NAME
+        ).addTypeConverter(Converters(GsonParser(Gson())))
+            .addTypeConverter(IMDBConverters(GsonParser(Gson())))
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun bindMovieCatalogRepository(api: MovieCatalogApi, imdbApi: IMDBApi, db: MovieCatalogDatabase):
+            MovieCatalogRepository = MovieCatalogRepository(nyApi = api, imdbApi = imdbApi, db = db)
+}
