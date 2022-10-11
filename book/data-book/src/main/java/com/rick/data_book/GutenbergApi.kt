055bab36dd18542450edabb78352139294f5fa67
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-11 10:02:59 +0300

    GutenbergApi.kt

--- book/data-book/src/main/java/com/rick/data_book/GutenbergApi.kt
+++ book/data-book/src/main/java/com/rick/data_book/GutenbergApi.kt
@@ -1,13 +1,23 @@
+import com.rick.data_book.GutenbergResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GutenbergApi {
     @GET("/books")
     suspend fun fetchBooks(
+    ): GutenbergResponseDto
 
-    )
+    @GET("/books/")
+    suspend fun getBook(
+        @Query("id") id: Int
+    ): GutenbergResponseDto
 
-//    @GET("/books")
-//    susp
+    @GET("/books/")
+    suspend fun searchBook(
+        @Query("?search") query: String
+    ): GutenbergResponseDto
 
+    companion object {
+        const val GUTENBERG_BASE_URL = "https://gutendex.com/"
+    }
 }
