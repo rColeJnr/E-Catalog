3ca05ec46017dae844c9ea2a7a2a850ebaa3e5d0
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 19:09:55 +0300

    AnimeCatalogAdapter.kt

---
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogAdapter.kt
@@ -0,0 +1,37 @@
+package com.rick.screen_anime
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.rick.data_anime.model_anime.Anime
+import com.rick.data_anime.model_manga.Manga
+import com.rick.screen_anime.manga_screen.MangaViewHolder
+
+class AnimeCatalogAdapter(
+    private val onItemClick: (view: View, anime: Anime) -> Unit
+) : PagingDataAdapter<Anime, MangaViewHolder>(DIFF_UTIL) {
+
+    override fun onBindViewHolder(holder: MangaViewHolder, position: Int) {
+        val anime = getItem(position)
+        if (anime != null) {
+            holder.bind(anime)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MangaViewHolder {
+        return MangaViewHolder.create(parent, onItemClick)
+    }
+
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Anime>() {
+            override fun areItemsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+                return oldItem.malId == newItem.malId
+            }
+
+            override fun areContentsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
--- anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogFragment.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogFragment.kt
@@ -1,10 +1,13 @@
 package com.rick.screen_anime
 
 import android.os.Bundle
+import android.view.AttachedSurfaceControl
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
 import com.rick.screen_anime.databinding.FragmentAnimeCatalogBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +16,9 @@ class AnimeCatalogFragment: Fragment() {
 
     private var _binding: FragmentAnimeCatalogBinding? = null
     private val binding get() = _binding!!
+private val viewModel: AnimeCatalogViewModel by viewModels()
+    private lateinit var adapter: AnimeCatalogAdapter
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
