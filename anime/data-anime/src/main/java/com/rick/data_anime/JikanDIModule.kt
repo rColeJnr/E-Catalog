453ac7b80d811b359d01abaf58f86115bd615b12
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 17:34:32 +0300

    JikanDIModule.kt

---
+++ anime/data-anime/src/main/java/com/rick/data_anime/JikanDIModule.kt
@@ -0,0 +1,50 @@
+package com.rick.data_anime
+
+import android.content.Context
+import androidx.room.Room
+import com.google.gson.Gson
+import com.rick.core.GsonParser
+import com.rick.data_anime.JikanApi.Companion.JIKAN_BASE_URL
+import com.rick.data_anime.JikanDatabase.Companion.JIKAN_DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+@InstallIn(SingletonComponent::class)
+@Module
+object JikanDIModule {
+
+    @Provides
+    fun providesJikanApi(): JikanApi = Retrofit.Builder()
+        .baseUrl(JIKAN_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+                .build()
+        )
+        .build()
+        .create()
+
+    @Provides
+    fun provideJikanDatabase(@ApplicationContext context: Context): JikanDatabase =
+        Room.databaseBuilder(
+            context,
+            JikanDatabase::class.java,
+            JIKAN_DATABASE_NAME
+        ).addTypeConverter(MangaConverters(GsonParser(Gson())))
+            .addTypeConverter(AnimeConverters(GsonParser(Gson())))
+            .build()
+
+    @Provides
+    fun provideJikanRepository(api: JikanApi, db: JikanDatabase): JikanRepository =
+        JikanRepository(api, db)
+
+}
