2ee8a58b2a696676ab48232e3fa4912049c08e4c
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 18:44:42 +0300

    MangaCatalogAdapter.kt done

--- anime/screen-anime/src/main/java/com/rick/screen_anime/manga_screen/MangaCatalogAdapter.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/manga_screen/MangaCatalogAdapter.kt
@@ -8,7 +8,7 @@ import com.rick.data_anime.model_manga.Manga
 
 class MangaCatalogAdapter(
     private val onItemClick: (view: View, manga: Manga) -> Unit
-): PagingDataAdapter<Manga, MangaViewHolder>(DIFF_UTIL) {
+) : PagingDataAdapter<Manga, MangaViewHolder>(DIFF_UTIL) {
 
     override fun onBindViewHolder(holder: MangaViewHolder, position: Int) {
         val manga = getItem(position)
@@ -18,11 +18,11 @@ class MangaCatalogAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MangaViewHolder {
-
+        return MangaViewHolder.create(parent, onItemClick)
     }
 
     companion object {
-        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Manga>(){
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Manga>() {
             override fun areItemsTheSame(oldItem: Manga, newItem: Manga): Boolean {
                 return oldItem.malId == newItem.malId
             }
