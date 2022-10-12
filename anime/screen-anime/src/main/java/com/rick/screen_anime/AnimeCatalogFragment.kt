a109ad5042bd956a11e9793ceca72c20717fe275
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 19:14:18 +0300

    toolbar and appbar configuration

--- anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogFragment.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogFragment.kt
@@ -1,24 +1,32 @@
 package com.rick.screen_anime
 
 import android.os.Bundle
-import android.view.AttachedSurfaceControl
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.transition.MaterialSharedAxis
 import com.rick.screen_anime.databinding.FragmentAnimeCatalogBinding
+import com.rick.screen_anime.manga_screen.MangaCatalogFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AnimeCatalogFragment: Fragment() {
+class AnimeCatalogFragment : Fragment() {
 
     private var _binding: FragmentAnimeCatalogBinding? = null
     private val binding get() = _binding!!
-private val viewModel: AnimeCatalogViewModel by viewModels()
+    private val viewModel: AnimeCatalogViewModel by viewModels()
     private lateinit var adapter: AnimeCatalogAdapter
     private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,7 +35,36 @@ private val viewModel: AnimeCatalogViewModel by viewModels()
     ): View? {
         _binding = FragmentAnimeCatalogBinding.inflate(inflater, container, false)
 
+        navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        view?.findViewById<Toolbar>(R.id.toolbar)
+            ?.setupWithNavController(navController, appBarConfiguration)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.jikan_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.search_jikan -> {
+                exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+                    duration = resources.getInteger(R.integer.catalog_motion_duration_long).toLong()
+                }
+                reenterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, false).apply {
+                    duration = resources.getInteger(R.integer.catalog_motion_duration_long).toLong()
+                }
+                val action = MangaCatalogFragmentDirections
+                    .actionMangaCatalogFragmentToSearchAnimeFragment()
+                navController.navigate(action)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onDestroy() {
