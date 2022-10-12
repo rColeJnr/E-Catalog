edc0268eb3b874bdac2a9461576aecc257b78b52
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 12:49:31 +0300

    AnimeRemoteMediator.kt Load remote keys

--- anime/data-anime/src/main/java/com/rick/data_anime/AnimeRemoteMediator.kt
+++ anime/data-anime/src/main/java/com/rick/data_anime/AnimeRemoteMediator.kt
@@ -6,11 +6,13 @@ import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import com.rick.data_anime.model_anime.Anime
 
+private const val STARTING_PAGE_INDEX = 1
+
 @OptIn(ExperimentalPagingApi::class)
 class AnimeRemoteMediator(
     private val api: JikanApi,
     private val db: JikanDatabase
-): RemoteMediator<Int, Anime>() {
+) : RemoteMediator<Int, Anime>() {
 
     override suspend fun initialize(): InitializeAction {
         // Launch remote refresh as soon as paging starts and do not trigger remote prepend or
@@ -49,34 +51,37 @@ class AnimeRemoteMediator(
 
         }
 
-        private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
-            // Get the last page that was retrieved, that contained items.
-            // From that last page, get the last item
-            return state.pages.lastOrNull() { it.data.isNotEmpty() }?.data?.lastOrNull()
-                ?.let { anime ->
-                    // Get the remote keys of the last item retrieved
-                    db.dao(anime.malId)
-                }
-        }
 
-        private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
-            // GEt the first page that was retrieved, that contained items.
-            // From that first page, get the first item
-            return state.pages.firstOrNull() { it.data.isNotEmpty() }?.data?.firstOrNull()
-                ?.let { anime ->
-                    // GEt the remote keys of the first items retrieved
-                    db.remoteKeysDao.remoteKeysId(anime.malId)
-                }
-        }
+    }
 
-        private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
-            // The paging library is trying to load data after the anchor position
-            // Get the item closest to the anchor position
-            return state.anchorPosition?.let { position ->
-                state.closestItemToPosition(position)?.malId?.let { anime ->
-                    db.remoteKeysDao.remoteKeysId(anime)
-                }
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
+        // Get the last page that was retrieved, that contained items.
+        // From that last page, get the last item
+        return state.pages.lastOrNull() { it.data.isNotEmpty() }?.data?.lastOrNull()
+            ?.let { anime ->
+                // Get the remote keys of the last item retrieved
+                db.animeRemoteKeysDao.remoteKeysId(anime.malId)
+            }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
+        // GEt the first page that was retrieved, that contained items.
+        // From that first page, get the first item
+        return state.pages.firstOrNull() { it.data.isNotEmpty() }?.data?.firstOrNull()
+            ?.let { anime ->
+                // GEt the remote keys of the first items retrieved
+                db.animeRemoteKeysDao.remoteKeysId(anime.malId)
+            }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, Anime>): AnimeRemoteKeys? {
+        // The paging library is trying to load data after the anchor position
+        // Get the item closest to the anchor position
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.malId?.let { anime ->
+                db.animeRemoteKeysDao.remoteKeysId(anime)
             }
         }
+    }
 
 }
