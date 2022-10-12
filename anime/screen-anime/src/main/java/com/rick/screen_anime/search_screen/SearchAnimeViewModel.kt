edbe320094399a8be4df14507bb530c5b1494e1d
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 19:22:04 +0300

    search actiona and state classesd

--- anime/screen-anime/src/main/java/com/rick/screen_anime/search_screen/SearchAnimeViewModel.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/search_screen/SearchAnimeViewModel.kt
@@ -9,4 +9,19 @@ import javax.inject.Inject
 class SearchAnimeViewModel @Inject constructor(
     private val repo: JikanRepository
 ) : ViewModel() {
+
+    val searchUiAction: (SearchUiAction) -> Unit
+    val searchUiState: (SearchUiState) -> Unit
+
+    init {
+
+    }
+
 }
+sealed class SearchUiAction {
+    data class SearchJikan(val query: String) : SearchUiAction()
+}
+
+data class SearchUiState(
+    val searchQuery: String? = null
+)
