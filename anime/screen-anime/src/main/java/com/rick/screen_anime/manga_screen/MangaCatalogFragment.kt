e13f308d7a4d13f2a1ba7e256a7231223371d094
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 18:49:41 +0300

    MangaCatalogFragment.kt base implementation

--- anime/screen-anime/src/main/java/com/rick/screen_anime/manga_screen/MangaCatalogFragment.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/manga_screen/MangaCatalogFragment.kt
@@ -6,8 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.rick.data_anime.model_manga.Manga
 import com.rick.screen_anime.databinding.FragmentMangaCatalogBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.Flow
 
 @AndroidEntryPoint
 class MangaCatalogFragment: Fragment() {
@@ -15,6 +21,7 @@ class MangaCatalogFragment: Fragment() {
     private var _binding: FragmentMangaCatalogBinding? = null
     private val binding get() = _binding!!
     private val viewModel: MangaCatalogViewModel by viewModels()
+    private lateinit var adapter: MangaCatalogAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,13 +32,37 @@ class MangaCatalogFragment: Fragment() {
 
         initAdapter()
 
+        binding.bindList(
+            viewModel.pagingDataFlow
+        )
+
         return binding.root
     }
 
     private fun initAdapter() {
-        adapter = MangaCatalogAdapter()
+        adapter = MangaCatalogAdapter(
+            this::onMangaClick
+        )
+
+        binding.recyclerView.itemAnimator = DefaultItemAnimator()
     }
 
+    private fun FragmentMangaCatalogBinding.bindList(pagingDataFlow: Flow<PagingData<Manga>>) {
+
+        lifecycleScope.launchWhenCreated {
+            pagingDataFlow.collect(adapter::submitData)
+        }
+
+    }
+
+    private fun onMangaClick(view: View, manga: Manga) {
+
+        val action = MangaCatalogFragmentDirections.actionMangaCatalogFragmentToDetailsAnimeFragment(
+            anime = null, manga = manga
+        )
+        findNavController().navigate(action)
+
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
