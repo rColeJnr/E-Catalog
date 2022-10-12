c0bee52b10b19546e7ac2035dbcf0f3b02b7e6a9
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 10:27:37 +0300

    implementation of libraries

--- anime/data-anime/build.gradle.kts
+++ anime/data-anime/build.gradle.kts
@@ -1,7 +1,29 @@
 apply {
     from("$rootDir/kotlin-library.gradle")
+    plugin("kotlin-kapt")
+    plugin("kotlin-parcelize")
+    plugin("dagger.hilt.android.plugin")
 }
 
 dependencies {
 
+    "implementation"(project(Modules.core))
+
+    "implementation"(Retrofit.retrofit)
+    "implementation"(Retrofit.converter)
+    "implementation"(Retrofit.okhttp)
+    "implementation"(Retrofit.interceptor)
+
+    "implementation"(Hilt.android)
+    "kapt"(Hilt.compiler)
+
+    "implementation"(Room.roomRuntime)
+    "implementation"(Room.room)
+    "implementation"(Room.paging)
+    "kapt"(Room.compiler)
+
+    "implementation"(Paging.paging)
+    "implementation"(Paging.runtime)
+
+    "implementation"(Coroutines.core)
 }
