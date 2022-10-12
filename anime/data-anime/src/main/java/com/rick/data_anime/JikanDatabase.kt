74a0a613ae5059aa17a4a3ca9d101024ca3b06b7
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 12:33:58 +0300

    JikanDatabase.kt

--- anime/data-anime/src/main/java/com/rick/data_anime/JikanDatabase.kt
+++ anime/data-anime/src/main/java/com/rick/data_anime/JikanDatabase.kt
@@ -2,8 +2,26 @@ package com.rick.data_anime
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.rick.data_anime.model_anime.Anime
+import com.rick.data_anime.model_manga.Manga
 
-@Database
+@Database(
+    entities = [
+        Manga::class,
+    Anime::class
+    ],
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters(MangaConverters::class, AnimeConverters::class)
 abstract class JikanDatabase: RoomDatabase() {
+
+    abstract val mangaDao: MangaDao
+    abstract val animeDao: AnimeDao
+
+    companion object {
+        const val JIKAN_DATABASE_NAME = "JIKAN_DB"
+    }
 
 }
