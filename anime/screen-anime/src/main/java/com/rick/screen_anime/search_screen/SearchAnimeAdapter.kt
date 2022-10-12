b974193d511ef83d940fbbed99d8ec656f046234
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 21:19:45 +0300

    SearchAnimeAdapter.kt done

--- anime/screen-anime/src/main/java/com/rick/screen_anime/search_screen/SearchAnimeAdapter.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/search_screen/SearchAnimeAdapter.kt
@@ -1,15 +1,40 @@
 package com.rick.screen_anime.search_screen
 
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.rick.data_anime.model_anime.Anime
-import com.rick.data_anime.model_manga.Manga
 
 class SearchAnimeAdapter(
-    private val onAnimeClick: (view: View, anime: Anime) -> Unit,
-    private val onMangaClick: (view: View, manga: Manga) -> Unit,
-): RecyclerView.Adapter<SearchMangaViewHolder>() {
+    private val onItemClick: (view: View, anime: Anime) -> Unit
+): RecyclerView.Adapter<SearchAnimeViewHolder>() {
 
-    
+    val differ = AsyncListDiffer(this, DIFF_UTIL)
 
+    override fun onBindViewHolder(holder: SearchAnimeViewHolder, position: Int) {
+        val anime = differ.currentList[position]
+        holder.bind(anime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchAnimeViewHolder {
+        return SearchAnimeViewHolder.create(parent, onItemClick)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<Anime>() {
+            override fun areItemsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+                return oldItem.malId == newItem.malId
+            }
+
+            override fun areContentsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 }
