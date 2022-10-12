0d739fb5224dd69569ac2d1eacc7f9d8bacd1ad6
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-12 19:13:02 +0300

    small erros fixed

--- anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogAdapter.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/AnimeCatalogAdapter.kt
@@ -5,22 +5,20 @@ import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import com.rick.data_anime.model_anime.Anime
-import com.rick.data_anime.model_manga.Manga
-import com.rick.screen_anime.manga_screen.MangaViewHolder
 
 class AnimeCatalogAdapter(
     private val onItemClick: (view: View, anime: Anime) -> Unit
 ) : PagingDataAdapter<Anime, AnimeViewHolder>(DIFF_UTIL) {
 
-    override fun onBindViewHolder(holder: MangaViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
         val anime = getItem(position)
         if (anime != null) {
             holder.bind(anime)
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MangaViewHolder {
-        return MangaViewHolder.create(parent, onItemClick)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
+        return AnimeViewHolder.create(parent, onItemClick)
     }
 
     companion object {
