6613e8331819f769549a2d4a00361b3ee8ae5047
rcolejnr <ricardo.m.jnr@gmail.com>
2022-10-13 13:28:53 +0300

    my 111 yesterday commits are finally working, with obviously some many UI bugs, but all easy fixes

--- anime/data-anime/src/main/java/com/rick/data_anime/model_manga/Prop.kt
+++ anime/data-anime/src/main/java/com/rick/data_anime/model_manga/Prop.kt
@@ -1,16 +1,14 @@
 package com.rick.data_anime.model_manga
 
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
-import android.os.Parcelable
 
 @Parcelize
 data class Prop(
     @SerializedName("from")
     val from: From,
     @SerializedName("to")
-    val to: To,
-    @SerializedName("string")
-    val string: String
+    val to: To
 ) : Parcelable
