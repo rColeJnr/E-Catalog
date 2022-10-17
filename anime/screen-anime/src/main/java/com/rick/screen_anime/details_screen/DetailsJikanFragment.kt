70eb2d3b18323a6d3c724995ff68e0b0d4ffeb83
rColeJnr <ricardo.m.jnr@gmail.com>
2022-10-17 23:36:00 +0300

    details screen polishing part 2

--- anime/screen-anime/src/main/java/com/rick/screen_anime/details_screen/DetailsJikanFragment.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/details_screen/DetailsJikanFragment.kt
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.findNavController
 import com.rick.data_anime.model_jikan.Jikan
 import com.rick.screen_anime.R
 import com.rick.screen_anime.databinding.FragmentJikanDetailsBinding
+import com.rick.screen_anime.getListAsString
 import com.rick.screen_anime.provideGlide
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -53,21 +54,25 @@ class DetailsJikanFragment : Fragment() {
 
         jikan.observe(viewLifecycleOwner) { anmeOrMnga ->
             title.text = anmeOrMnga.title
-            anmeOrMnga.images.jpg.imageUrl?.let { provideGlide(image, it) }
+            provideGlide(image, anmeOrMnga.images.jpg.imageUrl)
             synopsis.text = getString(R.string.synopsis, anmeOrMnga.synopsis)
-            background.text = anmeOrMnga.background
-            airingStatus.text = getString(R.string.airing_status, anmeOrMnga.status)
-            genreOne.text = anmeOrMnga.genres!!.firstOrNull()?.name ?: ""
-            genreTwo.text = anmeOrMnga.genres!!.getOrNull(1)?.name ?: ""
-            genreThree.text = anmeOrMnga.genres!!.getOrNull(2)?.name ?: ""
+            background.text = anmeOrMnga.background ?: getString(R.string.no_data)
+            genreOne.text =
+                anmeOrMnga.genres!!.firstOrNull()?.name ?: getString(R.string.no_data)
+            genreTwo.text =
+                anmeOrMnga.genres!!.getOrNull(1)?.name ?: getString(R.string.no_data)
+            genreThree.text =
+                anmeOrMnga.genres!!.getOrNull(2)?.name ?: getString(R.string.no_data)
             score.text = getString(R.string.score, anmeOrMnga.score)
             scoredBy.text = getString(R.string.scored_by, anmeOrMnga.scoredBy)
             rank.text = getString(R.string.rank, anmeOrMnga.rank)
             popularity.text = getString(R.string.popularity, anmeOrMnga.popularity)
             members.text = getString(R.string.members, anmeOrMnga.members)
             favorites.text = getString(R.string.favorites, anmeOrMnga.favorites)
+            url.text = getString(R.string.link_web_page, anmeOrMnga.url)
 
             if (anmeOrMnga.type == "TV") {
+                airingStatus.text = getString(R.string.airing_status, anmeOrMnga.status)
                 aired.text = getString(R.string.aired, anmeOrMnga.aired!!.string)
                 episodes.visibility = View.VISIBLE
                 episodes.text = getString(R.string.episodes, anmeOrMnga.episodes)
@@ -78,12 +83,12 @@ class DetailsJikanFragment : Fragment() {
                 pgRating.text = getString(R.string.pg_rating, anmeOrMnga.rating)
                 trailer.visibility = View.VISIBLE
                 trailer.text = getString(R.string.link_trailer, anmeOrMnga.trailer!!.url)
-                binding.url.text = getString(R.string.link_web_page, anmeOrMnga.url)
                 serializationLink.visibility = View.GONE
                 serializationName.visibility = View.GONE
                 serializations.visibility = View.GONE
             } else {
-                aired.text = getString(R.string.aired, anmeOrMnga.published?.string)
+                airingStatus.text = getString(R.string.publishing, anmeOrMnga.status)
+                aired.text = getString(R.string.published, anmeOrMnga.published?.string)
                 episodes.visibility = View.GONE
                 runtime.visibility = View.GONE
                 chapters.visibility = View.VISIBLE
@@ -91,7 +96,8 @@ class DetailsJikanFragment : Fragment() {
                 volumes.visibility = View.VISIBLE
                 volumes.text = getString(R.string.volumes, anmeOrMnga.volumes)
                 trailer.visibility = View.GONE
-                pgRating.text = getString(R.string.authors, "Antonieta")
+                pgRating.text =
+                    getString(R.string.authors, getListAsString(anmeOrMnga.authors!!.map { it!! }))
                 serializationLink.visibility = View.VISIBLE
                 serializationLink.text =
                     getString(
@@ -101,7 +107,7 @@ class DetailsJikanFragment : Fragment() {
                 serializationName.visibility = View.VISIBLE
                 serializationName.text =
                     getString(
-                        R.string.serialization_link,
+                        R.string.serialization_name,
                         anmeOrMnga.serializations!!.firstOrNull()?.name
                     )
                 serializations.visibility = View.VISIBLE
