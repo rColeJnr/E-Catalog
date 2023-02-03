e988ea063e07b3d87c8e53df22bbed042ce3a355
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-03 22:15:09 +0300

    Migrating to compose -> Replaced bookFragment error message with a composable

--- book/screen-book/src/main/java/com/rick/screen_book/BookCatalogFragment.kt
+++ book/screen-book/src/main/java/com/rick/screen_book/BookCatalogFragment.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.doOnPreDraw
@@ -190,11 +189,10 @@ class BookCatalogFragment : Fragment() {
 private fun ErrorMessage(message: String) {
     Text(text = message,
         style = MaterialTheme.typography.h5,
-        color = Color.Cyan,
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(horizontal = 8.dp)
-            .wrapContentWidth(Alignment.CenterHorizontally)
+            .wrapContentSize(align = Alignment.Center),
     )
 }
 
