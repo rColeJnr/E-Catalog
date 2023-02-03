c1b01968a2766d049e1bd5f35d086901121edd9e
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-03 20:59:54 +0300

    fixed typo in compose dependenciees

--- buildSrc/src/main/kotlin/Compose.kt
+++ buildSrc/src/main/kotlin/Compose.kt
@@ -1,15 +1,14 @@
 object Compose {
 
-    private val composeVersion = "1.1.1"
+    private const val composeVersion = "1.1.1"
 
-    val runtime = "androidx.compose.runtime:runtime:$composeVersion"
-    val ui = "androidx.compose.ui:ui:$composeVersion"
-    val foundation = "androidx.compose.foundation:foundation:$composeVersion"
-    val foundationLayout = "androidx.compose.foundation:foundation-layout:$composeVersion"
-    val material = "androidx.compose.material:material:$composeVersion"
-    val livedata = "androidx.compose.runtime:runtime-livedata:$composeVersion"
-    val tooling = "androidx.compose.ui:ui-tooling:$composeVersion"
+    const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
+    const val ui = "androidx.compose.ui:ui:$composeVersion"
+    const val foundation = "androidx.compose.foundation:foundation:$composeVersion"
+    const val foundationLayout = "androidx.compose.foundation:foundation-layout:$composeVersion"
+    const val material = "androidx.compose.material:material:$composeVersion"
+    const val livedata = "androidx.compose.runtime:runtime-livedata:$composeVersion"
+    const val tooling = "androidx.compose.ui:ui-tooling:$composeVersion"
 //    val themeAdapter = "com.google.accompanist:accompanist-themeadapter-material:$accompanistVersion"
-
 
 }
