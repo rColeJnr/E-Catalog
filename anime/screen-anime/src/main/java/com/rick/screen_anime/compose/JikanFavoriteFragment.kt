07489754d3263528331883266152facd79a89e68
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-13 00:39:09 +0300

    feat: favorites, jikan fav fragment created

---
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/compose/JikanFavoriteFragment.kt
@@ -0,0 +1,34 @@
+package com.rick.screen_anime.compose
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.accompanist.themeadapter.material.MdcTheme
+import com.rick.screen_anime.databinding.FragmentJikanFavoriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class JikanFavoriteFragment: Fragment() {
+
+    var _binding: FragmentJikanFavoriteBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentJikanFavoriteBinding.inflate(inflater, container, null)
+
+        binding.composeView.setContent {
+            MdcTheme {
+
+            }
+        }
+
+        return binding.root
+    }
+
+}
