9180ad99f783a7384e88cfc60af2602fdb8f1e54
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-14 23:28:42 +0300

    feat: JikanFavoriteFragment.kt, Jikan FavScreen Preview

--- anime/screen-anime/src/main/java/com/rick/screen_anime/compose/JikanFavoriteFragment.kt
+++ anime/screen-anime/src/main/java/com/rick/screen_anime/compose/JikanFavoriteFragment.kt
@@ -12,10 +12,13 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.google.accompanist.themeadapter.material.MdcTheme
+import com.rick.data_anime.model_jikan.Images
 import com.rick.data_anime.model_jikan.Jikan
+import com.rick.data_anime.model_jikan.Jpg
 import com.rick.screen_anime.databinding.FragmentJikanFavoriteBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -56,9 +59,10 @@ fun FavScreen(jikans: List<Jikan>) {
 
 @Composable
 fun JikanItem(jikan: Jikan) {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .wrapContentHeight()
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
     ) {
         Column(Modifier.fillMaxWidth()) {
             Text(text = jikan.title ?: "no title found")
@@ -69,3 +73,102 @@ fun JikanItem(jikan: Jikan) {
         }
     }
 }
+
+@Preview
+@Composable
+fun JikanItemPrev() {
+    FavScreen(jikans = dummyData)
+}
+
+private val dummyData = listOf(
+    Jikan(
+        0,
+        "",
+        Images(Jpg("", "", "")),
+        null,
+        "This dude title",
+        "anime",
+        "",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        0.0,
+        1,
+        1,
+        1,
+        1,
+        1,
+        "Def got nothing for you, but i'll keep writng so that we have at least on elong text, there's so much typos, and i am so tired i just want to sleep. i love me",
+        null,
+        null,
+        null,
+        null,
+        null,
+    ),
+    Jikan(
+        0,
+        "",
+        Images(Jpg("", "", "")),
+        null,
+        "This another title",
+        "anime",
+        "",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        0.0,
+        1,
+        1,
+        1,
+        1,
+        1,
+        "I do not have a synopsis for you",
+        null,
+        null,
+        null,
+        null,
+        null,
+    ),
+    Jikan(
+        0,
+        "",
+        Images(Jpg("", "", "")),
+        null,
+        "This another title",
+        "anime",
+        "",
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        0.0,
+        1,
+        1,
+        1,
+        1,
+        1,
+        "I may or may not have something for you",
+        null,
+        null,
+        null,
+        null,
+        null,
+    ),
+)
