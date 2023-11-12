768cf7c4c188c5b3019b1f941ba21fbbcf8fd5eb
rColeJnr <ricardo.m.jnr@gmail.com>
2023-11-12 15:53:28 +0300

    Delete book/screen-book/src/main/jni/book-keys.c

--- book/screen-book/src/main/jni/book-keys.c
+++
@@ -1,6 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL
-Java_com_rick_screen_1book_bestseller_1screen_BestsellerViewModelKt_getNYKey(JNIEnv *env, jclass clazz) {
-    return (*env)-> NewStringUTF(env, "r2hGllYW5TVGnqspbO8u3li1Un4AlQgQ");
-}
